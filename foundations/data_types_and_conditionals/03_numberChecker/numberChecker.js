126d5df1dc2ed48403dd0f8960a3c2af1d3b7cf9
Kamva <kamvainga5@gmail.com>
2026-09-06 23:02:00 +0000

    add if/else conditions to meet tests requirements

--- foundations/data_types_and_conditionals/03_numberChecker/numberChecker.js
+++ foundations/data_types_and_conditionals/03_numberChecker/numberChecker.js
@@ -1,5 +1,7 @@
 function numberChecker(number) {
-  if (number => 10) {
+  if (number == 9 || number == 6) {
+    return false;
+  }else if (number => 10) {
     return true;
   } else {
     return false;
